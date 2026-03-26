3af59e6d3b0929f7aae8ab70c67b11465f7bd0d7
Ez-zouek Hamza <ezzouekhamza2411@gmail.com>
2026-03-26 10:08:50 +0000

    feat: Implement face recognition to identify and label known faces with bounding boxes and names in an image.

--- FACE_recongnition/code_lab - 7.py
+++ FACE_recongnition/code_lab - 7.py
@@ -5,12 +5,12 @@ from PIL import Image, ImageDraw
 # avec encadrement est reconnaissance sur l'image du nom du personnage 
 
 # charger l'image de la personne est apprendre au système a la reconnaitre  .
-hakim_image = face_recognition.load_image_file("D:\\cours 2022\\ENSAM\\Cours IAGI 2 Intelligence Artificielle\\Labs\\Lab 2 - Vision intélligentes via OpenCV\\work\\Lab 8\\hakim.jpg")
+hakim_image = face_recognition.load_image_file("hakim.jpg")
 hakim_visage_signature = face_recognition.face_encodings(hakim_image)[0]
 
 
 # charger la deuxiéme image de personne est apprendre au système a la reconnaitre  .
-mrabet_image = face_recognition.load_image_file("D:\\cours 2022\\ENSAM\\Cours IAGI 2 Intelligence Artificielle\\Labs\\Lab 2 - Vision intélligentes via OpenCV\\work\\Lab 8\\Mrabet.jpg")
+mrabet_image = face_recognition.load_image_file("Mrabet.jpg")
 mrabet_visage_signature = face_recognition.face_encodings(mrabet_image)[0]
 
 # creer un tableau des signature des visages associés au nom de personnes
@@ -51,7 +51,10 @@ for (top, right, bottom, left), visage_signature in zip(visage_locations, visage
     img_dessin.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
 
     # dessiner un label avec le text du nom de visage reconnus
-    text_width, text_height = img_dessin.textsize(nom_personne)
+    # Utilisation de textbbox pour la compatibilité avec les versions récentes de Pillow
+    bbox = img_dessin.textbbox((0, 0), nom_personne)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
     img_dessin.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 255, 0), outline=(0, 0, 255))
     img_dessin.text((left + 6, bottom - text_height - 5), nom_personne, fill=(255, 0, 0, 255))
 
